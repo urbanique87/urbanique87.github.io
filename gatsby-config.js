d8ebfd88e577ad129ac2be6a105735a58bac8be4
urbanique87 <jungheeyov@gmail.com>
2021-07-16 00:35:39 +0900

    test: required image for gatsby-plugin-manifest?

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "",
+        icon: "src/images/icon.png",
       },
     },
     "gatsby-transformer-remark",
