b508b1fc9d15c0d6a82499664063cbae0d325464
urbanique87 <jungheeyov@gmail.com>
2021-07-16 00:18:51 +0900

    fix: remove icon img in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "",
       },
     },
     "gatsby-transformer-remark",
