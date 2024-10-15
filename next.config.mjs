d408803e2bb01c69490994d9efe31db3a47f8863
urbanique <jungheeyov@gmail.com>
2024-10-15 20:44:29 +0900

    chore: remove unused MDX-related packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
-import nextMDX from '@next/mdx'
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
 }
 
-export default withMDX(nextConfig)
+export default nextConfig
