ba086f416491555a4543456582994fbd936d048d
urbanique <jungheeyov@gmail.com>
2024-10-28 17:35:57 +0900

    feat: generate robots.txt file for SEO configuration

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next'
+// config
+import { BASE_URL } from '@/src/config/constants/paths'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+  }
+}
