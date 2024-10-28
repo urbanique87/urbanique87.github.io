a8f91b42dc56c1b6fcc598cb4c6852a0024e5046
urbanique <jungheeyov@gmail.com>
2024-10-28 17:33:51 +0900

    feat: add sitemap generation functionality

---
+++ src/app/sitemap.ts
@@ -0,0 +1,35 @@
+// types
+import type { MetadataRoute } from 'next'
+// config
+import { BASE_URL } from '@/src/config/constants/paths'
+// libs
+import { getCategoriesWithPostCount, getPosts } from '@/src/lib/post'
+
+export default async function generateSitemap(): Promise<MetadataRoute.Sitemap> {
+  const [{ categories }, posts] = await Promise.all([getCategoriesWithPostCount(), getPosts()])
+
+  // 카테고리 페이지 URL 생성
+  const categoryUrls = categories.map(({ category }) => {
+    const href = category === 'all' ? '/posts' : `/posts/${category}`
+
+    return {
+      url: `${BASE_URL}${href}`,
+      lastModified: new Date(),
+    }
+  })
+
+  // 포스트 페이지 URL 생성
+  const postUrls = posts.map(({ category, slug }) => ({
+    url: `${BASE_URL}/posts/${category}/${slug}`,
+    lastModified: new Date(),
+  }))
+
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+    },
+    ...categoryUrls,
+    ...postUrls,
+  ]
+}
--- src/config/constants/paths.ts
+++ src/config/constants/paths.ts
@@ -1,2 +1,5 @@
+// 애플리케이션의 기본 URL
+export const BASE_URL = 'https://urbanique87.github.io'
+
 // 포스트 디렉토리의 기본 경로
 export const POSTS_DIR = './src/posts'
